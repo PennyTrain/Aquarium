a6e200c0bdedf0cc8184349354e05c197a0601f2
Penny Train <penny.train04@gmail.com>
2024-01-18 14:20:33 +0000

    Linking up ElephantSQL

--- glassfish/settings.py
+++ glassfish/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cta04fd4bf*pd%xv_-lh#xyh6t3f7_0f2&zg$6+a8fvf-l00-q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
