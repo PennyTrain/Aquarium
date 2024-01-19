3289fd88cc163187826dc99281c8b5940589f960
Penny Train <penny.train04@gmail.com>
2024-01-19 17:05:07 +0000

    Created first model, added templates folder

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Posted"))
+
+class ReviewPost(models.Model):
+    title=models.CharField(max_length=100)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, 
+related_name="review_post"
+)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
