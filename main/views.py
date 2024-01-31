034a647d63e5abb33e1d135d2bd1517caae68c9f
Penny Train <penny.train04@gmail.com>
2024-01-31 16:04:04 +0000

    Add templates, styles, images, views.
    Add templates for visit page, home page and the gallery. Edit the base.html and add styling to all... Add images available for the gallery and the rest of the site. Along with editing the views enabling the templates to render soon.

--- main/views.py
+++ main/views.py
@@ -9,3 +9,18 @@ def index(request):
     template_name = 'main/index.html'
     return render(request, template_name)
 
+
+def gallery(request):
+    """
+    View that returns the gallery page
+    """
+    template_name = 'main/gallery.html'
+    return render(request, template_name)
+
+
+def visit(request):
+    """
+    View that returns the visit page
+    """
+    template_name = 'main/visit.html'
+    return render (request, template_name)
